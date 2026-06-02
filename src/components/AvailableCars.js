56d5bb97c62af98e080ff979c4da4deabe1482eb
Rafiul-goni-rayhan <rayhan.cse8.bu@gmail.com>
2026-06-02 13:37:36 +0600

    fetch data

--- src/components/AvailableCars.js
+++ src/components/AvailableCars.js
@@ -2,7 +2,7 @@ import React from 'react';
 import CarCard from './CarCard'; 
 
 const AvailableCars = async () => {
-    // ডাটাবেজ থেকে ডাটা নিয়ে আসা
+    //fetch data from database 
     const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/car`, {
        
     });
